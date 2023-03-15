fbb5a9964bffb73d721f75fa38131d46a495af9b
Nemo <me@captnemo.in>
2023-03-15 18:34:29 +0530

    Fix URL

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "/docs/api/kuvera/reference/Kuvera.yaml",
+    url: "reference/Kuvera.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
